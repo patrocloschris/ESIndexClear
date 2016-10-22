81b78d8e5634bdc1fefe6481661e5e495c2fd2f5
chrispat <christoupat@gmail.com>
2016-10-22 15:45:07 +0300

    Create method for the correct parameter validation

--- deleteIndicies.py
+++ deleteIndicies.py
@@ -1,11 +1,54 @@
 #!/usr/bin/python
 
-import sys,getopt
+import sys,getopt,re
+
+
+def check_parameters(esHost,indexFormat,interval):
+	#Check esHost
+	if esHost is None:
+		print 'Elastic search host is empty'
+		return false
+	host_split = esHost.split(":")
+	if len(host_split)>2:
+		print 'Elastic search host is not a proper url'
+		return false
+	res_match= re.match(r'[a-z][a-z0-9]+',host_split[0],re.I | re.M) 
+	if not res_match:
+		pat = re.compile("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
+		res_match = pat.match(host_split[0])
+		if not res_match:
+			print 'Elastic search host is not a valid domain'
+			return false
+	if len(host_split)==2:
+		res_match= re.match(r'^[0-9]+',host_split[1],re.I | re.M)	
+		if not res_match:
+		print 'Elastic search port is not a valid number'
+			return false
+	# Check indexFormat
+	if indexFormat is None:
+		print 'Index Format is empty'
+		return false
+	try:
+    	re.compile(indexFormat)
+	except re.error:
+		print 'Index Format can not be recognized as a regex string'
+		return false
+	#Check interval	
+	if interval is None:
+		print 'Interval is empty'
+		return false
+	res_match= re.match(r'^[1-9][0-9]*',intervalDays,re.I | re.M)	
+	if not res_match:
+		print 'Interval can not recognized as a valid number'
+		return false
+	return true	
+
+
 
 def main(filename,argv):
-	esHost = ''
-	indexFormat =  ''
-	intervalDays = 0
+	esHost = None
+	indexFormat =  None
+	intervalDays = None
 
 	# Get input parameters
 	try:
@@ -22,14 +65,19 @@ def main(filename,argv):
 		elif opt in ("-f"):
 			indexFormat = arg
 		elif opt in ("-i"):	
-			intervalDays = int(arg)
+			intervalDays = arg
 
-	#TODO check if user didn't set a parameter
-	#TODO check the right format of the parameters (if -e contains url and port, if interval is number etc)		
+	if not check_parameters(esHost,indexFormat,interval):
+		print 'Parameters:\n* ElasticSeach Host='+esHost+'\n* Index Format='+indexFormat+'\n* Interval Days='+intervalDays
+		sys.exit(2)
+	else:
+	print 'Starting with parameters:\n* ElasticSeach Host='+esHost+'\n* Index Format='+indexFormat+'\n* Interval Days='+str(intervalDays)
+		intervalDays = int(intervalDays)	
 
-	print 'Starting with parameters\n* ElasticSeach Host='+esHost+'\n* Index Format='+indexFormat+'\n* Interval Days='+str(intervalDays)
 
 	#Setup a connection to elastic search
+
+
 
 	#Create a ES client
 
